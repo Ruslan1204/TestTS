fbcc7d915b33f5a3a75be6e032d2b8dde4f508c1
Ruslan <ryslan12pess@ukr.net>
2024-10-02 20:25:05 +0300

    Practice

--- index.js
+++ index.js
@@ -1,11 +1,117 @@
-var srt = "Hello";
-console.log(srt);
-var userNameData = "Ruslan";
-var userAgeData = 36;
-var isBirthdayData = true;
-function logBrtSmg(isBirthday, userName, userAge) {
-    if (isBirthday) {
-        console.log("Congrats ".concat(userName.toUpperCase(), ", age: ").concat(userAge + 1));
+"use strict";
+// const srt = "Hello";
+// console.log(srt);
+// const userNameData: string = "Ruslan";
+// const userAgeData: number = 36;
+// const isBirthdayData: boolean = true;
+// function logBrtSmg(
+//   isBirthday: boolean,
+//   userName: string,
+//   userAge: number
+// ): void {
+//   if (isBirthday) {
+//     console.log(`Congrats ${userName.toUpperCase()}, age: ${userAge + 1}`);
+//   }
+// }
+// function logBrtSmg(
+//   isBirthday: boolean,
+//   userName: string,
+//   userAge: number
+// ): string {
+//   if (isBirthday) {
+//     return `Congrats ${userName.toUpperCase()}, age: ${userAge + 1}`;
+//   } else return "error";
+// }
+// const userNameData: string = "Ruslan";
+// const userAgeData: number = 36;
+// const isBirthdayData: boolean = true;
+// const userData =
+//   '{"isBirthdayData": true, "userNameData": "Ruslan", "userAgeData": 36}';
+// const userObj: {
+//   isBirthdayData: boolean;
+//   userNameData: string;
+//   userAgeData: number;
+// } = JSON.parse(userData);
+// const logBrtSmg = (
+//   isBirthday: boolean,
+//   userName: string,
+//   userAge: number
+// ): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName.toUpperCase()}, age: ${userAge + 1}`;
+//   } else return "error";
+// };
+// logBrtSmg(isBirthdayData, userNameData, userAgeData);
+// const userNameData: string = "Ruslan";
+// const userAgeData: number = 36;
+// const isBirthdayData: boolean = true;
+// const userData = {
+//   isBirthdayData: true,
+//   userNameData: "Ruslan",
+//   userAgeData: 36,
+//   messages: {
+//     error: "Error",
+//   },
+// };
+// const createError = (msg: string) => {
+//   throw new Error(msg);
+// };
+// const logBrtSmg = ({
+//   isBirthdayData,
+//   userNameData,
+//   userAgeData,
+//   messages: { error },
+// }: {
+//   isBirthdayData: boolean;
+//   userNameData: string;
+//   userAgeData: number;
+//   messages: { error: string };
+// }): string => {
+//   if (isBirthdayData) {
+//     return `Congrats ${userNameData.toUpperCase()}, age: ${userAgeData + 1}`;
+//   } else return createError(error);
+// };
+// logBrtSmg(userData);
+// const departments: string[] = ["dev", "design", "marketing"];
+// const num: number[][] = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+// ];
+// const report = departments
+//   .filter((d: string) => d !== "dev")
+//   .map((d: string) => `${d} - done`);
+// const [first] = report;
+// console.log(first);
+const electricityUserData = {
+    readings: 95,
+    units: "kWt",
+    mode: "double",
+};
+const waterUserData = {
+    readings: 3,
+    units: "m3",
+};
+const elRate = 0.45;
+const wRate = 2;
+const monthPayments = [0, 0]; // [electricity, water]
+const calculatePayments = ({ readings, mode }, wData, elRate, wRate) => {
+    if (mode === "double" && readings < 50) {
+        monthPayments[0] = readings * elRate * 0.7;
     }
-}
-logBrtSmg(isBirthdayData, userNameData, userAgeData);
+    else {
+        monthPayments[0] = readings * elRate;
+    }
+    monthPayments[1] = wData.readings * wRate;
+};
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+const sendInvoice = ([el, water], electricityUserData, waterUserData) => {
+    const text = `    Hello!
+      This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+      It will cost: ${el}€
+      
+      This month you used ${waterUserData.readings} ${waterUserData.units} of waterts-node index.ts
+      It will cost: ${water}€`;
+    return text;
+};
+const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
+console.log(invoice);
--- index.ts
+++ index.ts
@@ -50,39 +50,101 @@
 
 // logBrtSmg(isBirthdayData, userNameData, userAgeData);
 
-const userNameData: string = "Ruslan";
-const userAgeData: number = 36;
-const isBirthdayData: boolean = true;
+// const userNameData: string = "Ruslan";
+// const userAgeData: number = 36;
+// const isBirthdayData: boolean = true;
 
-const userData = {
-  isBirthdayData: true,
-  userNameData: "Ruslan",
-  userAgeData: 36,
-  messages: {
-    error: "Error",
-  },
+// const userData = {
+//   isBirthdayData: true,
+//   userNameData: "Ruslan",
+//   userAgeData: 36,
+//   messages: {
+//     error: "Error",
+//   },
+// };
+
+// const createError = (msg: string) => {
+//   throw new Error(msg);
+// };
+
+// const logBrtSmg = ({
+//   isBirthdayData,
+//   userNameData,
+//   userAgeData,
+//   messages: { error },
+// }: {
+//   isBirthdayData: boolean;
+//   userNameData: string;
+//   userAgeData: number;
+//   messages: { error: string };
+// }): string => {
+//   if (isBirthdayData) {
+//     return `Congrats ${userNameData.toUpperCase()}, age: ${userAgeData + 1}`;
+//   } else return createError(error);
+// };
+
+// logBrtSmg(userData);
+
+// const departments: string[] = ["dev", "design", "marketing"];
+// const num: number[][] = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+// ];
+
+// const report = departments
+//   .filter((d: string) => d !== "dev")
+//   .map((d: string) => `${d} - done`);
+
+// const [first] = report;
+// console.log(first);
+
+const electricityUserData = {
+  readings: 95,
+  units: "kWt",
+  mode: "double",
 };
 
-const createError = (msg: string) => {
-  throw new Error(msg);
+const waterUserData = {
+  readings: 3,
+  units: "m3",
 };
 
-const logBrtSmg = ({
-  isBirthdayData,
-  userNameData,
-  userAgeData,
-  messages: { error },
-}: {
-  isBirthdayData: boolean;
-  userNameData: string;
-  userAgeData: number;
-  messages: { error: string };
-}): string => {
-  if (isBirthdayData) {
-    return `Congrats ${userNameData.toUpperCase()}, age: ${userAgeData + 1}`;
-  } else return createError(error);
+const elRate: number = 0.45;
+const wRate: number = 2;
+
+const monthPayments: number[] = [0, 0]; // [electricity, water]
+
+const calculatePayments = (
+  { readings, mode }: { readings: number; mode: string },
+  wData: { readings: number },
+  elRate: number,
+  wRate: number
+) => {
+  if (mode === "double" && readings < 50) {
+    monthPayments[0] = readings * elRate * 0.7;
+  } else {
+    monthPayments[0] = readings * elRate;
+  }
+
+  monthPayments[1] = wData.readings * wRate;
 };
 
-logBrtSmg(userData);
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
 
-const departments = ["dev", "design", "marketing"];
+const sendInvoice = (
+  [el, water]: number[],
+  electricityUserData: { readings: number; units: string },
+  waterUserData: { readings: number; units: string }
+) => {
+  const text = `    Hello!
+      This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+      It will cost: ${el}€
+      
+      This month you used ${waterUserData.readings} ${waterUserData.units} of waterts-node index.ts
+      It will cost: ${water}€`;
+
+  return text;
+};
+
+const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
+console.log(invoice);
